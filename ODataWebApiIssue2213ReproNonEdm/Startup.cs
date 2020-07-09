a5ddcd1aafe7dcabb506f835a77a8ec4f01f6e5e
eduardoxvii <eduardogs@bol.com.br>
2020-07-09 12:38:10 -0300

    Add files via upload

---
+++ ODataWebApiIssue2213ReproNonEdm/Startup.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ODataWebApiIssue2213ReproNonEdm.Data;
+using ODataWebApiIssue2213ReproNonEdm.Models;
+using System;
+using System.Linq;
+
+namespace ODataWebApiIssue2213ReproNonEdm
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(this.Configuration);
+            //services.AddDbContext<InMemoryDbContext>(options =>
+            //options.UseMySql("server=localhost;database=shool;uid=shool;pwd=Stlt1ls_iy").EnableSensitiveDataLogging());//PARA O POMELO!
+            //options.UseMySql("server=10.200.2.197;database=faetecWebApi;uid=faetecHomolog;pwd=6r!xe*isp7n=CRu").EnableSensitiveDataLogging());//PARA O POMELO!
+            services.AddDbContext<InMemoryDbContext>(options => options.UseInMemoryDatabase(databaseName: "NonEdm2213Db"));
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddOData();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            
+            // Non-Edm Approach
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.EnableDependencyInjection();
+                endpoints.Select().Filter().Expand().Count().MaxTop(10);
+            });
+
+            using (IServiceScope serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                InMemoryDbContext db = serviceScope.ServiceProvider.GetRequiredService<InMemoryDbContext>();
+                Company company=null;
+                if (!db.Companies.Any())
+                {
+                    var shippingAddress = new StreetAddress { Id = 3, Street = "Company 2", City = "City 1" };
+                    db.Streetaddresses.Add(shippingAddress);
+                    company = new Company { Id = 2, Name = "Company 1", ManagerNage = "Blá Blá", StreetAddress = shippingAddress };
+                    db.Companies.Add(company);
+
+                    db.SaveChanges();
+                }
+
+                if (!db.Schools.Any())
+                {
+                    var shippingAddress = new StreetAddress { Id = 1, Street = "Street 1", City = "City 1" };
+                    db.Streetaddresses.Add(shippingAddress);
+
+                    var order = new Order { Id = 1, Description = "Order 1 Description " };
+                    db.Orders.Add(order);
+
+                    db.Schools.Add(new School { Id = 1, Branch = company, Name = "School 1", StreetAddress = shippingAddress, Order = order });
+
+                    db.SaveChanges();
+                }
+
+                /*if (!db.Users.Any())
+                {
+                    db.Users.AddRange(
+                        new User { Id = 1, Name = "John Doe", CreatedOn = new DateTimeOffset(DateTime.Parse("2020-06-24T15:57:44.3780001+03:00")) },
+                        new User { Id = 2, Name = "Foo Bar", CreatedOn = new DateTimeOffset(DateTime.Now) });
+
+                    db.SaveChanges();
+                }*/
+            }
+        }
+    }
+}
